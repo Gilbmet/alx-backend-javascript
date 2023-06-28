cc6c2b9f46356f8169d6e57850e1bf1cff604f49
Gilbmet <gilbmet254@gmail.com>
2023-06-28 12:12:18 +0300

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,16 +1,4 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Your API logic here
-    // You can make asynchronous calls, such as fetching data from an API
-    
-    // If the API call is successful, resolve the promise with the response data
-    const response = { message: "API response" };
-    resolve(response);
-    
-    // If there's an error in the API call, reject the promise with an error message
-    // const error = "API error";
-    // reject(error);
-  });
+/* eslint-disable */
+export default function getResponseFromAPI() {
+  return (new Promise((resolve, reject) => {}));
 }
-
-export default getResponseFromAPI;
